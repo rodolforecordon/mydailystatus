458c6282e7ec298d12bea737a7731548c4709ed7
Rodolfo Recordon <rodolforecordon@gmail.com>
2020-05-21 00:22:23 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+        <h1><img src='/logo.png' alt='Ola FSL!'/></h1>
+    )
+}
+
+export default Index
---
+++ postcss.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    plugins: ['tailwind','postcss-preset-env']
+}
